586526ca007de5186e25e914fcf5183dfca7f34c
Sixto-Castro93 <sjcastro@espol.edu.ec>
2018-04-16 16:37:57 -0500

    CSS y getClientIp

--- app.js
+++ app.js
@@ -1604,7 +1604,7 @@ MongoClient.connect(url, function(err, db) {
         } else if (result.length > 0) {
 
             var url2 = JSON.parse(lzstring.decompressFromBase64(result[0].json)).IP_Servidor_Chat_Broadcast.url
-   getExternalIp(function(externalIp) {
+   getClientIp(function(externalIp) {
                     console.log(externalIp)
                     if(externalIp.localeCompare("localhost")!=0)
                     {
@@ -1748,6 +1748,24 @@ function getExternalIp(cb) {
         return cb(body);
     });
 }
+
+function getClientIp(req) {
+  var ipAddress;
+  // The request may be forwarded from local web server.
+  var forwardedIpsStr = req.header('x-forwarded-for'); 
+  if (forwardedIpsStr) {
+    // 'x-forwarded-for' header may return multiple IP addresses in
+    // the format: "client IP, proxy 1 IP, proxy 2 IP" so take the
+    // the first one
+    var forwardedIps = forwardedIpsStr.split(',');
+    ipAddress = forwardedIps[0];
+  }
+  if (!ipAddress) {
+    // If request was not forwarded
+    ipAddress = req.connection.remoteAddress;
+  }
+  return ipAddress;
+};
 
   function screen(path,res,req,ban) {
   var stat = fs.statSync(path);
